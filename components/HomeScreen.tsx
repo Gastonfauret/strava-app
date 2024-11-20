eb4ca7cc32449a3d24ed9dd2984bb1f6e535b815
Gastonfauret <Gastonfauret@gmail.com>
2024-11-20 12:05:34 -0300

    Google Auth is not working

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import GoogleLogin from './GoogleLogin';
 
 export default function HomeScreen() {
     return (
         <>
             <View style={styles.container}>
-                <Text>Hola, Que haces?</Text>
+                <GoogleLogin/>
+                <Image
+                resizeMode='contain'                
+                style={styles.homeImg}
+                source={require('../assets/images/Strava-Home.webp')}/>
             </View>
         </>
-
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',        
-        paddingHorizontal: 120,
-        backgroundColor: 'red'
+        justifyContent: 'center',     
     },
+
+    homeImg: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    }
 });
 
 
